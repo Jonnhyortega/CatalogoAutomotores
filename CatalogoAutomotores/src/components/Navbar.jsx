ac6b8629a39e1775257bbcbfc7bd5d4f42c52ff2
Jonathan Ortega <132107501+Jonnhyortega@users.noreply.github.com>
2024-08-01 11:28:43 -0300

    update

--- CatalogoAutomotores/src/components/Navbar.jsx
+++ CatalogoAutomotores/src/components/Navbar.jsx
@@ -2,23 +2,27 @@ import React, { useState } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { stock } from "../Js/autos";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCar } from '@fortawesome/free-solid-svg-icons';
 
 const MyNavbar = ({ onCarSelect }) => {
-  const [isNavOpen, setIsNavOpen] = useState(false); 
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   const handleCarSelect = (event, car) => {
-    event.stopPropagation(); 
+    event.stopPropagation();
     onCarSelect(car);
-    setIsNavOpen(false); 
+    setIsNavOpen(false);
   };
 
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+    <Navbar bg="black" variant="dark" expand="lg" fixed="top">
       <Container>
         <Navbar.Toggle
           aria-controls="basic-navbar-nav"
           onClick={() => setIsNavOpen(!isNavOpen)}
-        />
+        >
+          <FontAwesomeIcon icon={faCar} style={{ color: 'white'}} />
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav" in={isNavOpen}>
           <Nav className="me-auto">
             {stock.map((car, index) => (
